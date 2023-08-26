2d0f8b4726798e79ef6fd9507b054f98eae11a36
Kim jihwan <kjhwan0802@naver.com>
2023-08-26 20:41:41 +0900

    Feat : 페이지 라우팅
    - 경로, 파일 정리
    - page (Home, Roominfo, Search) 추가
    - component (Header 추가)

--- sublet-front/src/App.js
+++ sublet-front/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Index from './pages/Home';
+import RoomInfo from './pages/RoomInfo';
+import Search from './pages/Search';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           {/* 기본 페이지 */}
-          <Route path="/"/>
+          <Route path="/" element={<Index />}/>
+          <Route path="/roominfo" element={<RoomInfo />}/>
+          <Route path="/search" element={<Search />}/>
         </Routes>
       </BrowserRouter>
   );
