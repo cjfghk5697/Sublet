455939d80d9fbbfd63884b48fe98800bf60078ab
unknown <cjfghk5697@gmail.com>
2023-09-23 17:30:25 -0400

    get 기능 완료

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,21 +1,37 @@
-import { Controller, Get, Req, Param, UseGuards } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get, Req, Param, UseGuards, NotFoundException, Post, Body, BadRequestException} from '@nestjs/common';
 import { LoggedInGuard } from '../../guards/logged-in.guard';
+import { UserCreateDto } from './dto/user.dto';
+import { MongodbService } from '../mongodb/mongodb.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly db: MongodbService) {}
 
-  //@UseGuards(LoggedInGuard)
-  @Get()
-  getAllUser() {
-    return this.userService.getAllUser();
-  }
+  // @UseGuards(LoggedInGuard)
+  // @Get()
+  // async getAllUser() {
+  //   return this.userService.getAllUser();
+  // }
 
   @Get(':user_id')
-  getOneUser(@Param('user_id') user_id: string) {
-    return this.userService.getUserByKey(user_id);
+  async getOneUser(@Param('user_id') user_id: string) {
+    const res = await this.db.getOneUser(user_id);
+    if (!res){
+      console.log('res is empty,', res);
+      throw new NotFoundException();
+    }
+    console.log('res found,', res);
+    return res;
   }
 
+  @Post()
+  async createUser(@Body() data: UserCreateDto){
+    console.log('data:', data);
+    try{
+      return await this.db.createUser
+    }catch(e){
+      throw new BadRequestException();
+    }
 
+  }
 }
