22c5fe28b981784af7ed3882c68e1fd535166311
Kim jihwan <kjhwan0802@naver.com>
2023-11-01 22:03:14 +0900

    [ADD] NAVER MAP API

--- sublet-front/src/pages/RoomInfo.js
+++ sublet-front/src/pages/RoomInfo.js
@@ -4,11 +4,13 @@ import PersonIcon from '@mui/icons-material/Person';
 import SingleBedIcon from '@mui/icons-material/SingleBed';
 import HomeIcon from '@mui/icons-material/Home';
 import BathtubIcon from '@mui/icons-material/Bathtub';
+import React, { useState, useEffect } from 'react';
 
 const roomTempData = makeTest.makeTestData(); // This is a temporary data for testing
 
 export default function RoomInfo() {
   const nowRomeNum = 0; //추후 prop으로 받아오면 될듯
+  //const API_KEY = process.env.REACT_APP_NAVERMAP_API_KEY;
 
   const styles = {
     RomeInfo_ImgContainer: {
@@ -46,6 +48,38 @@ export default function RoomInfo() {
     },
   };
 
+  const [mapPoint, setMapPoint] = useState({ x: null, y: null });
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    const mapDiv = document.getElementById("map");
+    const map = new window.naver.maps.Map(mapDiv);
+
+    //지도에서 위치 클릭하면 도로명 주소로 받아오는 것 test
+    window.naver.maps.Event.addDOMListener(mapDiv, "click", () => {
+      const coordinate = { x: map.data.map.center.x, y: map.data.map.center.y };
+      setMapPoint({ x: coordinate.x, y: coordinate.y });
+      window.naver.maps.Service.reverseGeocode(
+        {
+          coords: new window.naver.maps.LatLng(coordinate.y, coordinate.x),
+          orders: [
+            window.naver.maps.Service.OrderType.ADDR,
+            window.naver.maps.Service.OrderType.ROAD_ADDR,
+          ].join(","),
+        },
+        (status, response) => {
+          if (status !== window.naver.maps.Service.Status.OK) {
+            return alert("Something wrong!");
+          }
+          const result = response.v2;
+          setLocation(result.address.jibunAddress);
+          console.log(result);
+        }
+      );
+    });
+  }, []);
+
+
   return (
     <div>
       <Header />
@@ -68,6 +102,7 @@ export default function RoomInfo() {
         <HomeIcon />
         <BathtubIcon />
       </div>
+      <div id="map" style={{ width: "400px", height: "400px" }} />
     </div >
   );
 }
