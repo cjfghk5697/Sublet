ec0e79a64120c89195c0eeb691160e65cc5561f9
Kim jihwan <kjhwan0802@naver.com>
2023-11-16 20:55:35 +0900

    ADD front pages

--- sublet-front/src/App.js
+++ sublet-front/src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
 import Home from './pages/Home';
 import RoomInfo from './pages/RoomInfo';
-import Search from './pages/Search';
+import SaveSubletInfo from './pages/SaveSubletInfo';
+import ReHome from './pages/ReHome';
+import Booking from './pages/Booking';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
+import * as makeTest from './testdata/testdata.js'
+
 function App() {
+  const roomTempData = makeTest.makeTestData(); // This is a temporary data for testing
+
   return (
-      <BrowserRouter>
-        <Routes>
-          {/* 기본 페이지 */}
-          <Route path="/" element={<Home />}/>
-          <Route path="/roominfo" element={<RoomInfo />}/>
-          <Route path="/search" element={<Search />}/>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        {/* 기본 페이지 */}
+        <Route path="/" element={<Home />} />
+        <Route path="/roominfo" element={<RoomInfo />} />
+        <Route path="/SaveSubletInfo" element={<SaveSubletInfo room={roomTempData[0]} />} />
+        <Route path="/ReHome" element={<ReHome />} />
+        <Route path="/Booking" element={<Booking />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- sublet-front/src/pages/Home.js
+++ sublet-front/src/pages/Home.js
@@ -2,6 +2,7 @@ import RoomProfile from '../components/RoomProfile';
 import Header from '../components/Header';
 import React from 'react';
 import * as makeTest from '../testdata/testdata.js'
+import { Link } from 'react-router-dom';
 
 const roomTempData = makeTest.makeTestData(); // This is a temporary data for testing
 
@@ -21,7 +22,7 @@ export default function Home() {
     },
   };
 
-  let rooms = roomTempData.map( (room) => {
+  let rooms = roomTempData.map((room) => {
     return (
       <RoomProfile
         room={room}
@@ -31,7 +32,12 @@ export default function Home() {
 
   return (
     <div style={styles.container}>
-        <Header />
+      {/* 임시 페이지들 링크*/}
+      <Link to="/RoomInfo">RoomInfo</Link>
+      <Link to="/SaveSubletInfo">SaveSubletInfo</Link>
+      <Link to="/ReHome">ReHome</Link>
+      <Link to="/Booking">Booking</Link>
+      <Header />
       <div style={styles.roomContainer}>
         {rooms}
       </div>
