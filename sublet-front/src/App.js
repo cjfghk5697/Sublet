85d2d06425a83f46ec125e48b712bed55523ad42
김지환 (Ji-hwan Kim) <kjhwan0802@naver.com>
2024-01-24 12:58:31 +0000

    createsubletinfo  post 작성하는 페이지 임시로 만드는 중

--- sublet-front/src/App.js
+++ sublet-front/src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import Home from './pages/Home';
 import RoomInfo from './pages/RoomInfo';
-import SaveSubletInfo from './pages/SearchSubletInfo';
+import SearchSubletInfo from './pages/SearchSubletInfo';
 import ReHome from './pages/ReHome';
 import Booking from './pages/Booking';
 import GuestInfo from './pages/GuestInfo.js'
 import LoginPage from './pages/Login.js';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-
-import * as makeTest from './testdata/testdata.js'
+import CreateSubletInfo from './pages/Host/CreateSubletInfo.js';
 
 function App() {
-  const roomTempData = makeTest.makeTestData(); // This is a temporary data for testing
 
   return (
     <BrowserRouter>
@@ -19,11 +17,13 @@ function App() {
         {/* 기본 페이지 */}
         <Route path="/" element={<Home />} />
         <Route path="/roominfo" element={<RoomInfo />} />
-        <Route path="/SearchSubletInfo" element={<SaveSubletInfo room={roomTempData[0]} />} />
+        <Route path="/SearchSubletInfo" element={<SearchSubletInfo />} />
         <Route path="/ReHome" element={<ReHome />} />
         <Route path="/Booking" element={<Booking />} />
         <Route path='/GuestInfo' element={<GuestInfo />} />
         <Route path='/login' element={<LoginPage />} />
+        {/* test host page */}
+        <Route path='/CreateSubletInfo' element={<CreateSubletInfo />} />
       </Routes>
     </BrowserRouter>
   );
