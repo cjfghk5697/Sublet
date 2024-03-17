f3a2360026190628f93a3e341e34542c76606485
김지환 (Ji-hwan Kim) <kjhwan0802@naver.com>
2024-03-17 08:04:35 +0000

    RoomInfo component 분리 및 style 분리

--- sublet-front/src/components/styles/RoomInfo.styles.js
+++ sublet-front/src/components/styles/RoomInfo.styles.js
@@ -8,3 +8,15 @@ export const ImgContainer = styled.div`
   justify-content: center;
   height: 300px;
 `;
+
+export const RoomInfoSection = styled.section`
+  ${tw`bg-white p-4 rounded-lg shadow-md mx-3 mb-6`}
+`;
+
+export const RoomTitle = styled.section`
+  ${tw`text-3xl font-bold mx-3 mt-1 mb-6`}
+`;
+
+export const RoomInfoButton = styled.button`
+  ${tw`w-full rounded-lg bg-gray-300 text-black p-1`}
+`;
